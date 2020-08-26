ea498c5d441577f0ae13a1f10a617d78943f8b65
Jesus Torres <jstorres@pucp.pe>
2020-08-26 06:52:29 -0500

    Finishing exploration for base dataframe

---
+++ 1WebScrappingExploration.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 14:36:30 2020
+
+@author: JTR
+"""
+#%% Ruta y librerias
+my_folder = r'C:\Users\LENOVO\Desktop\pokemon'
+
+import pandas as pd
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import os
+import re
+import shutil
+
+#%% Exploring html and defining generations
+url = 'https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number'
+
+page = requests.get(url)
+print(page)
+
+bs = BeautifulSoup(page.content)
+html = str(bs)
+
+#We can get to this exploring the web page
+l = html.split('Pokémon\n</th>')
+print(len(l))
+for j,i in enumerate(l):
+    print(j)
+    print(i[:500])
+    print('---')
+#%% See where we end
+end = l=html.split('id="See_also"')
+print(len(end))
+
+for i in end:
+    print(i[:100])
+    print('---')
+    
+interest = end[0]
+#%% Separacion de generaciones
+
+generations = interest.split('Pokémon\n</th>')
+print(len(generations))
+
+for i in generations:
+    print(i[:500])
+    print('---')
+# La primera se debe dropear, los demas funcionan
+generations = generations[1:]
+
+
+#%% Exploratorio por generacion
+aux1 = re.findall('<tr style="background:#FFF">\n<td style="font-family:monospace">([^<]+)\n</td>',generations[0])
+print('kdex number:')
+print(aux1[-10:])
+print(len(aux1))
+aux2 = re.findall('<td style="font-family:monospace">([^<]+)\n</td>\n<th>',generations[0])
+print('Ndex number:')
+print(aux2[-10:])
+print(len(aux2))
+aux3 = re.findall('<th> <a href="([^<]+)" title="',generations[0])
+print('link:')
+print(aux3[-10:])
+print(len(aux3))
+print('nombre:')
+aux4 = [i.split('/')[2].split('_')[0] for i in aux3]
+print(aux4[-10:])
+print(len(aux4))
+#Type and stats can be downloaded from their respective links
+"""
+Observations
+1. There is a problem in the length of the first generation: Moltres, Zapdos and Articuno
+2. There is a problem with Slowking in the 2nd
+"""
+
+
